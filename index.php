97e290c20740f021f79f6bf1e361a3db61798364
zhou37263585 <57888983+zhou37263585@users.noreply.github.com>
2019-12-11 16:55:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -774,8 +774,8 @@ function render_list($path, $files)
                 <div id="upload_div" style="margin:10px">
                 <center>
 			<input id="upload_file" type="file" name="upload_filename" onchange="document.getElementById('flieText').value = this.value" style="display:none">
-			<input value="<?php echo $constStr['Upload'][$constStr['language']]; ?>" type="button" onclick="document.getElementById('upload_file').click();">
-			<input id="flieText" type="text" style="border:0;outline:none;" onclick="document.getElementById('upload_file').click();" value="<?php echo $constStr['Upload'][$constStr['language']]; ?>">
+			<input value="<?php echo $constStr['FileSelected'][$constStr['language']]; ?>" type="button" onclick="document.getElementById('upload_file').click();">
+			<input id="flieText" type="text" style="border:0;outline:none;" onclick="document.getElementById('upload_file').click();" value="<?php echo $constStr['NoFileSelected'][$constStr['language']]; ?>">
 			<input id="upload_submit" onclick="preup();" value="<?php echo $constStr['Upload'][$constStr['language']]; ?>" type="button">
                 <center>
                 </div>
