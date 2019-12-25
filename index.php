e23948539ba147cdc587c2c8d753023987b75170
zhou37263585 <57888983+zhou37263585@users.noreply.github.com>
2019-12-25 10:36:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1110,7 +1110,7 @@ function render_list($path, $files)
 		<?php } ?>
 <?php   } else if($_SERVER['user']){ ?>
 	<a onclick="userLoginOut()" class="userLoginOut_a">
-				<svg t="1577089283125" class="icon operate_ico" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2711" width="16" height="16"><path d="M972.8 512l-307.2-256 0 153.6-358.4 0 0 204.8 358.4 0 0 153.6 307.2-256zM153.6 153.6l409.6 0 0-102.4-409.6 0c-56.32 0-102.4 46.08-102.4 102.4l0 716.8c0 56.32 46.08 102.4 102.4 102.4l409.6 0 0-102.4-409.6 0 0-716.8z" p-id="2712" fill="#1296db"></path></svg>
+				<svg t="1577089283125" class="icon userLoginOut_ico" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2711" width="16" height="16"><path d="M972.8 512l-307.2-256 0 153.6-358.4 0 0 204.8 358.4 0 0 153.6 307.2-256zM153.6 153.6l409.6 0 0-102.4-409.6 0c-56.32 0-102.4 46.08-102.4 102.4l0 716.8c0 56.32 46.08 102.4 102.4 102.4l409.6 0 0-102.4-409.6 0 0-716.8z" p-id="2712" fill="#1296db"></path></svg>
 				<?php echo $constStr['Logout'][$constStr['language']]; ?></a>
 
  <?php   } else { ?>
